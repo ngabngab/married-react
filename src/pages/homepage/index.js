ab0598b0b92509f4c511c326dd02ae6a0ac69cb8
Bambang Setiyawan <bse@voxteneo.asia>
2019-11-13 00:42:45 +0700

    preloading screen

---
+++ src/pages/homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ReactComponent as NameIconSvg} from '../../assets/img/name.svg'
+
+const HomePage = () => {
+  return (
+      <div className="pre-loading-screen">
+        <NameIconSvg />
+      </div>
+  )
+}
+
+export default HomePage;
