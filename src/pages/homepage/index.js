bbf6a610820d5f129c50f296ebec66c9581fc213
Bambang Setiyawan <bse@voxteneo.asia>
2019-11-14 00:01:32 +0700

    Animation preloading

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {ReactComponent as NameIconSvg} from '../../assets/img/name.svg'
 
 const HomePage = () => {
+
+  useEffect(() => {
+    document.querySelectorAll('#Layer_1 path').forEach((e) => {
+      console.log(e.getTotalLength())
+    })
+  }, [])
+
   return (
       <div className="pre-loading-screen">
         <NameIconSvg />
