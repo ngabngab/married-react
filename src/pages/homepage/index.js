3d4dc34cafb6460bd5262f8739a95d04c9386485
Bambang Setiyawan <bse@voxteneo.asia>
2019-11-14 00:23:22 +0700

    zoom out animation

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,16 +1,18 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import {ReactComponent as NameIconSvg} from '../../assets/img/name.svg'
 
 const HomePage = () => {
 
+  const preLoadingScreenContainer = useRef();
+
   useEffect(() => {
-    document.querySelectorAll('#Layer_1 path').forEach((e) => {
-      console.log(e.getTotalLength())
-    })
+    setTimeout(() => {
+      preLoadingScreenContainer.current.classList.add('zoom-out')
+    }, 6000);
   }, [])
 
   return (
-      <div className="pre-loading-screen">
+      <div className="pre-loading-screen" ref={preLoadingScreenContainer}>
         <NameIconSvg />
       </div>
   )
